d29617b557847c9423f08ec389d8ae15b5a7e02e
ChrisesDK <115223371+ChrisesDK@users.noreply.github.com>
2023-11-23 13:50:43 +0100

    hasBought color implemented

--- Assets/Scripts/BorderColorChanger.cs
+++ Assets/Scripts/BorderColorChanger.cs
@@ -6,10 +6,10 @@ using UnityEngine.UI;
 public class BorderColorChanger : MonoBehaviour
 {
     private GameManager gameManager;
-    private Color bought = new Color(200, 200, 200, 255);
-    private Color canAfford = new Color(0, 255, 0, 255);
-    private Color canAlmostAfford = new Color(255, 255, 0, 255);
-    private Color canNotAfford = new Color(255, 0, 0, 255); 
+    public Color bought;
+    public Color canAfford;
+    public Color canAlmostAfford;
+    public Color canNotAfford;
 
     void Start()
     {
@@ -22,17 +22,24 @@ public class BorderColorChanger : MonoBehaviour
         {
             for (int j = 1; j < transform.GetChild(i).childCount; j++)
             {
-                if(transform.GetChild(i).GetChild(j).transform.GetComponent<Policy>().price < gameManager.money)
+                if (transform.GetChild(i).GetChild(j).transform.GetComponent<Policy>().isPurchased)
                 {
-                    transform.GetChild(i).GetChild(j).GetChild(1).GetChild(0).transform.GetComponent<Image>().color = canAfford;
-                }
-                else if(transform.GetChild(i).GetChild(j).transform.GetComponent<Policy>().price < gameManager.money * 2)
-                {
-                    transform.GetChild(i).GetChild(j).GetChild(1).GetChild(0).transform.GetComponent<Image>().color = canAlmostAfford;
+                    transform.GetChild(i).GetChild(j).GetChild(1).GetChild(0).transform.GetComponent<Image>().color = bought;
                 }
                 else
                 {
-                    transform.GetChild(i).GetChild(j).GetChild(1).GetChild(0).transform.GetComponent<Image>().color = canNotAfford;
+                    if(transform.GetChild(i).GetChild(j).transform.GetComponent<Policy>().price < gameManager.money)
+                    {
+                        transform.GetChild(i).GetChild(j).GetChild(1).GetChild(0).transform.GetComponent<Image>().color = canAfford;
+                    }
+                    else if(transform.GetChild(i).GetChild(j).transform.GetComponent<Policy>().price < gameManager.money * 2)
+                    {
+                        transform.GetChild(i).GetChild(j).GetChild(1).GetChild(0).transform.GetComponent<Image>().color = canAlmostAfford;
+                    }
+                    else
+                    {
+                        transform.GetChild(i).GetChild(j).GetChild(1).GetChild(0).transform.GetComponent<Image>().color = canNotAfford;
+                    }
                 }
             }
         }
